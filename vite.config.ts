cafaea0b5bd6c1d5b90135fe441b64999c4acd37
Akezhan <akidulatini@gmail.com>
2023-12-18 15:19:18 +0600

    Initial configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    plugins: [
+        // @ts-ignore
+        handlebars(), 
+    ],
     build: {
         outDir: 'dist'
     },
