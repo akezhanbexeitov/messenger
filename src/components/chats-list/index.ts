09a9f8c3bcf1182e27a1e8f4f5eabd7e3440c4bb
Akezhan <akidulatini@gmail.com>
2023-12-20 16:17:27 +0600

    Create chats page

--- src/components/chats-list/index.ts
+++ src/components/chats-list/index.ts
@@ -38,6 +38,74 @@ Handlebars.registerHelper('chats', () => {
             avatar: avatar,
             unread: 5,
             time: "15:30"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 2,
+            time: "12:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            time: "13:45"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 1,
+            time: "16:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            active: true,
+            time: "21:05"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 5,
+            time: "15:30"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 2,
+            time: "12:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            time: "13:45"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 1,
+            time: "16:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            active: true,
+            time: "21:05"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 5,
+            time: "15:30"
         }
     ]
 })
