16d98bb7ac68213743a1b8d51da9b8d377ee5339
Akezhan <akidulatini@gmail.com>
2023-12-20 13:43:35 +0600

    Complete ChatsCard component

--- src/components/chats-list/index.ts
+++ src/components/chats-list/index.ts
@@ -1,1 +1,43 @@
-export { default as ChatsList } from './chats-list.hbs?raw';
+import avatar from "../../assets/avatar.png"
+import Handlebars from "handlebars"
+
+export { default as ChatsList } from './chats-list.hbs?raw'
+
+Handlebars.registerHelper('chats', () => {
+    return [
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 2,
+            time: "12:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            time: "13:45"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 1,
+            time: "16:00"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            active: true,
+            time: "21:05"
+        },
+        {
+            name: "Андрей",
+            message: "Здравствуйте",
+            avatar: avatar,
+            unread: 5,
+            time: "15:30"
+        }
+    ]
+})
