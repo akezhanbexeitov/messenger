cfebb60452eb7dcfcc2166c89bb7e092dbfa8b13
Akezhan <akidulatini@gmail.com>
2023-12-21 11:39:24 +0600

    Make Field component different for auth page and profile page

--- src/components/field/index.ts
+++ src/components/field/index.ts
@@ -1,1 +1,11 @@
+import Handlebars from 'handlebars';
+
 export { default as Field } from './field.hbs?raw'
+
+Handlebars.registerHelper('isAuthField', (value) => {
+  return value === 'auth'
+});
+
+Handlebars.registerHelper('isProfileField', (value) => {
+  return value === 'profile'
+});
