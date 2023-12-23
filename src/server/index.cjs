5d4e56fa31b937f1dd19815b6c1e8e25ace02237
Akezhan <akidulatini@gmail.com>
2023-12-23 23:18:56 +0600

    Try another way to access static files with Express

--- src/server/index.cjs
+++ src/server/index.cjs
@@ -4,11 +4,8 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
+app.use(express.static(path.join(__dirname, '../../dist/')))
 app.use(express.static(path.join(__dirname, '../assets/')))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
-});
 
 app.listen(PORT, () => { 
     console.log('Server is running on port ' + PORT)
