8121dd27c1a0a31239b276751364c5a18836b77a
Akezhan <akidulatini@gmail.com>
2024-01-02 11:46:06 +0600

    Boost typescript performance and add ts checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
     plugins: [
         // @ts-ignore
         handlebars(), 
+        checker({
+            typescript: true
+        })
     ],
     build: {
         outDir: 'dist'
