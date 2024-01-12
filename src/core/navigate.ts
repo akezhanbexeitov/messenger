b101ed02bc1a98a02a38d82660a1746626d27faa
Akezhan <akidulatini@gmail.com>
2024-01-12 15:44:37 +0600

    Uncomment routes to navigate function

--- src/core/navigate.ts
+++ src/core/navigate.ts
@@ -5,11 +5,11 @@ const pages = {
   'register': Pages.RegisterPage,
   'chats': Pages.ChatsPage,
   'chat': Pages.ChatPage,
-  // 'profile': Pages.ProfilePage,
-  // 'change-profile': Pages.ChangeProfilePage,
-  // 'change-password': Pages.ChangePasswordPage,
-  // '404': Pages.NotFoundPage,
-  // '500': Pages.InternalServerErrorPage,
+  'profile': Pages.ProfilePage,
+  'change-profile': Pages.ChangeProfilePage,
+  'change-password': Pages.ChangePasswordPage,
+  '404': Pages.NotFoundPage,
+  '500': Pages.InternalServerErrorPage,
 };
 
 export function navigate(page: string): void {
