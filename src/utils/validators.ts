4d5ac3708be971892e2fa000fc6f3e0c9ada9fe8
Akezhan <akidulatini@gmail.com>
2024-01-13 15:45:37 +0600

    Write validators for first name and last name

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,10 +1,56 @@
-export const login = (value: string) => {
-    if (!value) {
-        return 'Field can not be empty'
+export const first_name = (value: string) => {
+    // Check if value contains latin or cyrillic characters
+    const cyrillicPattern = /^[а-яА-ЯёЁ\s]+$/
+    const latinPattern = /^[a-zA-Z\s]+$/
+    if (!cyrillicPattern.test(value) || !latinPattern.test(value)) { 
+        return 'First name should contain only Cyrillic or Latin characters'
     }
 
-    if (value.length < 3) {
-        return `Length of login should not be less 3 letters.`
+    // Check if first letter is not a capital letter
+    const firstLetterNotCapitalPattern = /^[^A-ZА-ЯЁ]/
+    if (firstLetterNotCapitalPattern.test(value)) {
+        return 'First letter should be a capital letter'
+    }
+
+    // Check if value contains any spaces or numbers
+    const spacesOrNumbersPattern = /\s|\d/
+    if (spacesOrNumbersPattern.test(value)) {
+        return 'Login should not contain any spaces or numbers'
+    }
+
+    // Check if value contains any special signs
+    const specialSignsPattern = /-/;
+    if (specialSignsPattern.test(value)) {
+        return 'Value should not contain any special signs'
+    }
+
+    return false
+}
+
+export const second_name = (value: string) => {
+    // Check if value contains latin or cyrillic characters
+    const cyrillicPattern = /^[а-яА-ЯёЁ\s]+$/
+    const latinPattern = /^[a-zA-Z\s]+$/
+    if (!cyrillicPattern.test(value) || !latinPattern.test(value)) { 
+        return 'First name should contain only Cyrillic or Latin characters'
+    }
+
+    // Check if first letter is not a capital letter
+    const firstLetterNotCapitalPattern = /^[^A-ZА-ЯЁ]/
+    if (firstLetterNotCapitalPattern.test(value)) {
+        return 'First letter should be a capital letter'
+    }
+
+    // Check if value contains any spaces or numbers
+    const spacesOrNumbersPattern = /\s|\d/
+    if (spacesOrNumbersPattern.test(value)) {
+        return 'Login should not contain any spaces or numbers'
+    }
+
+    // Check if value contains any special signs
+    const specialSignsPattern = /-/;
+    if (specialSignsPattern.test(value)) {
+        return 'Value should not contain any special signs'
     }
 
     return false
