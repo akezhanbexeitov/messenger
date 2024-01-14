3a78d51ae9b44b7540c1be5f7abcc71baca6ea81
Akezhan <akidulatini@gmail.com>
2024-01-14 15:49:30 +0600

    Fix first name and second name validators

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,60 +1,23 @@
-export const first_name = (value: string) => {
+const namesValidators = (value: string) => {
     // Check if value contains latin or cyrillic characters
     const cyrillicPattern = /^[а-яА-ЯёЁ\s]+$/
     const latinPattern = /^[a-zA-Z\s]+$/
-    if (!cyrillicPattern.test(value) || !latinPattern.test(value)) { 
-        return 'First name should contain only Cyrillic or Latin characters'
+    if (!cyrillicPattern.test(value) && !latinPattern.test(value)) { 
+        return 'First name must contain only Cyrillic or Latin characters'
     }
 
     // Check if first letter is not a capital letter
     const firstLetterNotCapitalPattern = /^[^A-ZА-ЯЁ]/
     if (firstLetterNotCapitalPattern.test(value)) {
-        return 'First letter should be a capital letter'
-    }
-
-    // Check if value contains any spaces or numbers
-    const spacesOrNumbersPattern = /\s|\d/
-    if (spacesOrNumbersPattern.test(value)) {
-        return 'First name should not contain any spaces or numbers'
-    }
-
-    // Check if value contains any special signs
-    const specialSignsPattern = /-/;
-    if (specialSignsPattern.test(value)) {
-        return 'First name should not contain any special signs'
+        return 'First letter must be a capital letter'
     }
 
     return false
 }
 
-export const second_name = (value: string) => {
-    // Check if value contains latin or cyrillic characters
-    const cyrillicPattern = /^[а-яА-ЯёЁ\s]+$/
-    const latinPattern = /^[a-zA-Z\s]+$/
-    if (!cyrillicPattern.test(value) || !latinPattern.test(value)) { 
-        return 'Second name should contain only Cyrillic or Latin characters'
-    }
+export const first_name = (value: string) => namesValidators(value)
 
-    // Check if first letter is not a capital letter
-    const firstLetterNotCapitalPattern = /^[^A-ZА-ЯЁ]/
-    if (firstLetterNotCapitalPattern.test(value)) {
-        return 'First letter should be a capital letter'
-    }
-
-    // Check if value contains any spaces or numbers
-    const spacesOrNumbersPattern = /\s|\d/
-    if (spacesOrNumbersPattern.test(value)) {
-        return 'Second name should not contain any spaces or numbers'
-    }
-
-    // Check if value contains any special signs
-    const specialSignsPattern = /-/;
-    if (specialSignsPattern.test(value)) {
-        return 'Value should not contain any special signs'
-    }
-
-    return false
-}
+export const second_name = (value: string) => namesValidators(value)
 
 export const login = (value: string) => {
     // Check if the login is not from 3 to 20 characters long
