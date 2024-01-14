922ee589613e36310e4346175f62ab52a914e0a7
Akezhan <akidulatini@gmail.com>
2024-01-14 16:07:00 +0600

    Add styles for profile errors, add validation

--- src/pages/profile/change-password/change-password.ts
+++ src/pages/profile/change-password/change-password.ts
@@ -2,6 +2,7 @@ import { Field } from "../../../components";
 import Block from "../../../core/Block";
 import { PAGES, navigate } from "../../../core/navigate";
 import template from './change-password.hbs?raw'
+import * as validators from "../../../utils/validators"
 
 interface IProps { }
 
@@ -14,6 +15,11 @@ type TRef = {
 export class ChangePasswordPage extends Block<IProps, TRef> {
     constructor() {
         super({
+            validate: {
+                oldPassword: validators.password,
+                newPassword: validators.password,
+                repeatNewPassword: validators.password
+            },
             handleBackClick: () => { 
                 navigate(PAGES.PROFILE)
             },
@@ -21,6 +27,7 @@ export class ChangePasswordPage extends Block<IProps, TRef> {
                 const oldPassword = this.refs.oldPassword.value()
                 const newPassword = this.refs.newPassword.value()
                 const repeatNewPassword = this.refs.repeatNewPassword.value()
+                if (!oldPassword || !newPassword || !repeatNewPassword) return
                 console.log({
                     oldPassword,
                     newPassword,
