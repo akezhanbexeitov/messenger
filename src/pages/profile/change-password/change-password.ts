24c66c4770275064ddb5e404ce51c2203f0ac7c6
Akezhan <akidulatini@gmail.com>
2024-01-14 13:43:04 +0600

    Get all values from change password form

--- src/pages/profile/change-password/change-password.ts
+++ src/pages/profile/change-password/change-password.ts
@@ -1,14 +1,31 @@
+import { Field } from "../../../components";
 import Block from "../../../core/Block";
 import { PAGES, navigate } from "../../../core/navigate";
 import template from './change-password.hbs?raw'
 
-interface IProps {}
+interface IProps { }
 
-export class ChangePasswordPage extends Block<IProps> {
+type TRef = {
+    oldPassword: Field
+    newPassword: Field
+    repeatNewPassword: Field
+}
+
+export class ChangePasswordPage extends Block<IProps, TRef> {
     constructor() {
         super({
             handleBackClick: () => { 
                 navigate(PAGES.PROFILE)
+            },
+            handleSaveChangesClick: () => {
+                const oldPassword = this.refs.oldPassword.value()
+                const newPassword = this.refs.newPassword.value()
+                const repeatNewPassword = this.refs.repeatNewPassword.value()
+                console.log({
+                    oldPassword,
+                    newPassword,
+                    repeatNewPassword
+                })
             }
         })
     }
