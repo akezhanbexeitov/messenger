d8094a8083e8c0608b0357b119bb87bb05a473cb
Akezhan <akidulatini@gmail.com>
2024-01-31 16:53:49 +0600

    Add event listener to avatar

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -1,9 +1,25 @@
 import Block from "../../core/Block";
 import template from "./avatar.hbs?raw"
 
-interface IProps {}
+interface IProps { 
+    events: {
+        change: (event: Event) => void
+    }
+}
 
-export class Avatar extends Block<IProps> {
+type TRefs = {
+    avatarRef: HTMLInputElement
+}
+
+export class Avatar extends Block<IProps, TRefs> {
+    protected init() {
+        this.props.events = {
+            change: () => {
+                console.log("SOmething happened")
+            }
+        }
+    }
+
     protected render(): string {
         return template
     }
