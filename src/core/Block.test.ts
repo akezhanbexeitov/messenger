3d1d87e2204737c43cfc8b4731b6d1c33bca81e0
Akezhan <akidulatini@gmail.com>
2024-02-20 14:38:36 +0600

    Download sinon and write tests for Block

--- src/core/Block.test.ts
+++ src/core/Block.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai"
 // eslint-disable-next-line import/extensions
 import Block, { Events } from "./Block.ts"
+import sinon from "sinon"
 
 interface Props {
   text?: string
@@ -41,6 +42,48 @@ describe("Block", () => {
     const element = component.element as HTMLDivElement
     const elementValue = element.querySelector("#test-text")?.innerHTML
 
-    expect(elementValue).to.include("Hello world")
+    expect(elementValue).to.be.equal(text)
+  })
+
+  it("should have reactive behavior", () => {
+    const text = "Hello world"
+    const component = new ComponentClass({ text })
+
+    const newText = "New hello world"
+    component.setProps({ text: newText })
+    const element = component.element as HTMLDivElement
+    const elementValue = element.querySelector("#test-text")?.innerHTML
+
+    expect(elementValue).to.be.equal(newText)
+  })
+
+  it("should handle events properly", () => {
+    const stub = sinon.stub()
+    const component = new ComponentClass({
+      events: {
+        click: stub
+      }
+    })
+
+    const clickEvent = new MouseEvent("click")
+    const element = component.element as HTMLDivElement
+    element.dispatchEvent(clickEvent)
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    expect(stub.calledOnce).to.be.true
+  })
+
+  it("should call dispatchComponentDidMount method", () => { 
+    const clock = sinon.useFakeTimers()
+    const component = new ComponentClass()
+
+    const spy = sinon.spy(component, 'componentDidMount')
+
+    const element = component.getContent()
+    document.body.appendChild(element!)
+    clock.next()
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    expect(spy.calledOnce).to.be.true
   })
 })
